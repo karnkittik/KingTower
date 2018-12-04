895762f44a16696f520c27e4a569d105899f261d
karnkittik <karnkitti_tang@hotmail.com>
2018-12-04 18:31:11 +0700

    Commit

--- src/Home.java
+++ src/Home.java
@@ -1,13 +1,15 @@
 
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Home extends Application {
  
 	@Override
 	public void start(Stage primaryStage) {
-		Button n = new Button();
+		Button n = new Button("Hello");
+		
 	}
 
 	public static void main(String[] args) {
