6558ea0554c26361b4438415accff1f38b0417d1
karnkittik <karnkitti_tang@hotmail.com>
2018-12-04 20:51:27 +0700

    Test1 canvas

--- src/Home.java
+++ src/Home.java
@@ -1,16 +1,32 @@
 
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Home extends Application {
  
 	@Override
 	public void start(Stage primaryStage) {
-		Button n = new Button("Hello");
+		StackPane root = new StackPane();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Test1");
 		
-||||||| merged common ancestors
+		Canvas canvas = new Canvas(600,800);
+		root.getChildren().add(canvas);
+		
+		
+		
+		
+		
+		
+		
+		primaryStage.show();
+		
 		
 	}
 
