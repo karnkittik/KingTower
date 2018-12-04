2c46fe62b9bba58ba5771d3d1828b302fac570a0
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-12-04 18:19:42 +0700

    create button

--- src/Home.java
+++ src/Home.java
@@ -1,12 +1,13 @@
 
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Home extends Application {
-
+ 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		Button n = new Button();
 	}
 
 	public static void main(String[] args) {
