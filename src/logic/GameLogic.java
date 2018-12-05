de7314a71b0ff065be3f141fc5ca292e7dd34a19
karnkittik <karnkitti_tang@hotmail.com>
2018-12-05 09:49:47 +0700

    Create gamelogic

---
+++ src/logic/GameLogic.java
@@ -0,0 +1,11 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class GameLogic {
+	private ArrayList<Character> party;
+	private ArrayList<Character> activeCharacters;
+	private Character boss;
+	
+
+}
